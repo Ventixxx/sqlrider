58bc7aaa2dec3f86dbd56b141607c4c2033e08b4
Ventixxx <itsVentixxx@gmail.com>
2025-06-02 16:45:38 +0200

    first commit

---
+++ src/main/java/dev/ventixxx/sqlrider/SQLRider.java
@@ -0,0 +1,16 @@
+package dev.ventixxx.sqlrider;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SQLRider extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
